5855c006172a82fc1ce4af83e022fb03e751bb72
LucyLedezma <lucialedezmasilva@gmail.com>
2023-02-05 05:13:38 +0000

    lambdas functions added!

---
+++ .ipynb_checkpoints/lambda1-checkpoint.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import base64
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+    
+    # Get the s3 address from the Step Function event input
+    key = event['key'] #"train/bicycle_s_000038.png" ## TODO: fill in
+    bucket = event['bucket']  #"lucialedezma-workflow-project" ## TODO: fill in
+    
+    # Download the data from s3 to /tmp/image.png
+    ## TODO: fill in
+    s3.download_file(bucket, key, '/tmp/image.png')
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
